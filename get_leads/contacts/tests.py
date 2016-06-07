3f196ce384c2d09adc7dafffa33ac455c3956eb0
Sermsook Pulnil <sermsook@PRONTOMAC0012.local>
2016-06-07 14:34:44 +0700

    Create contact model with firstname and lastname

--- get_leads/contacts/tests.py
+++ get_leads/contacts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Contact
 
 # Create your tests here.
+class ContactTest(TestCase):
+	def test_contact_should_have_firstname_and_lastname(self):
+		contact = Contact()
+		contact.firstname = 'Steve'
+		contact.lastname = 'Roger'
+		contact.save()
+		contact = Contact.objects.first()
+		self.assertEqual(contact.firstname, 'Steve')
+		self.assertEqual(contact.lastname, 'Roger')
+
