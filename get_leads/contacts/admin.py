b65f378d7d4f005c6e2abdfb0250c97c8dd7b0c0
mesodiar <burasakorn_12@prontomarketing.com>
2016-06-13 14:44:01 +0700

    Create email column in admin

--- get_leads/contacts/admin.py
+++ get_leads/contacts/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('firstname','lastname')
+    list_display = ('firstname','lastname','email')
 admin.site.register(Contact, ContactAdmin)
